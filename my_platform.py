f3aba099ad88f9eaa58dde256992b98bbc343189
Siyue <sy2321@ic.ac.uk>
2023-11-29 18:19:21 +0000

    create the database model and define functions for creating new tasks

--- my_platform.py
+++ my_platform.py
@@ -1,8 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  # Path to the database file
+db = SQLAlchemy(app)
+
+# define the database model
+class TrainingTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(50), default='pending')
+    parameters = db.Column(db.JSON)  # You can store parameters as JSON
+
+    def __repr__(self):
+        return '<TrainingTask %r>' % self.id
+
+def create_training_task(parameters):
+    """
+    Business Logic:
+    Create a new training task with the given parameters.
+
+    :param parameters: A dictionary containing the parameters for the training task.
+    :return: The created TrainingTask instance.
+    """
+    new_task = TrainingTask(parameters=parameters)
+    db.session.add(new_task)
+    db.session.commit()
+    return new_task
+
+@app.route('/create_task', methods=['POST'])
+def create_task_endpoint():
+    data = request.json
+    parameters = data.get('parameters')
+    new_task = create_training_task(parameters)
+    return jsonify({"message": "Task created", "task_id": new_task.id}), 201
+
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-#tmr ijlk
+@app.route("/home")
+def home():
+    return render_template('home.html')
 
+if __name__ == '__main__':
+    app.run(debug = True)
