861a73690b9d1c505f734b600d2f8273319c8efd
Siyue <sy2321@ic.ac.uk>
2023-12-24 14:56:04 +0000

    create the database ddl and define functions for creating new tasks

--- my_platform.py
+++ my_platform.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  # Path to the database file
@@ -8,11 +9,21 @@ db = SQLAlchemy(app)
 # define the database model
 class TrainingTask(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    created_time = db.Column(db.DateTime, default=datetime.utcnow)
     status = db.Column(db.String(50), default='pending')
     parameters = db.Column(db.JSON)  # You can store parameters as JSON
 
     def __repr__(self):
         return '<TrainingTask %r>' % self.id
+
+class TrainingResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_time = db.Column(db.DateTime, default=datetime.utcnow)
+    accuracy = db.Column(db.Float, nullable=True)
+    precision = db.Column(db.Float, nullable=True)
+
+    def __repr__(self):
+        return '<TrainingResult %r>' % self.id
 
 def create_training_task(parameters):
     """
@@ -33,6 +44,20 @@ def create_task_endpoint():
     parameters = data.get('parameters')
     new_task = create_training_task(parameters)
     return jsonify({"message": "Task created", "task_id": new_task.id}), 201
+
+def train_task(id):
+
+
+@app.route('/start_training_task', methods=['POST'])
+def start_training():
+    data = request.json
+    parameters = data.get('parameters', {})
+    # Create and start the training task
+    new_task = create_training_task(parameters)
+    train_task(new_task.id)  # Function to start the training process
+    return jsonify({"message": "Training started", "task_id": new_task.id}), 201
+
+
 
 
 
